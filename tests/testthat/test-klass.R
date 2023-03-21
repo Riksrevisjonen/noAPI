df6956ec31e45feca04e5505761933aae0e3c1e1
Aleksander Eilertsen <a.eilertsen@pm.me>
2023-03-21 12:21:48 +0100

    Add get_klass_codes_safe
    Closes #35

--- tests/testthat/test-klass.R
+++ tests/testthat/test-klass.R
@@ -1,7 +1,22 @@
 # ---- Test for warnings and errors ----
 test_that('get_municipality() issue warnings and errors as expected', {
-  expect_error(get_municipalities(year = 1776))
-  expect_error(get_municipalities('abc'))
+  expect_message(expect_null(get_municipalities(year = 1776)))
+  expect_message(expect_null(get_municipalities('abc')))
+})
+
+test_that('get_countries() issue warnings and errors as expected', {
+  expect_message(expect_null(get_countries(year = 1776)))
+  expect_message(expect_null(get_countries('abc')))
+})
+
+test_that('get_counties() issue warnings and errors as expected', {
+  expect_message(expect_null(get_counties(year = 1776)))
+  expect_message(expect_null(get_counties('abc')))
+})
+
+test_that('get_adm_units() issue warnings and errors as expected', {
+  expect_message(expect_null(get_adm_units(year = 1776)))
+  expect_message(expect_null(get_adm_units('abc')))
 })
 
 # Skip rest of tests if live API test are disabled
@@ -138,4 +153,11 @@ test_that('get_counties() returns expected results', {
   expect_equal(names(df[[1]]), c('url', 'status', 'content', 'response'))
   expect_identical(class(df[[1]]), "noAPI")
   expect_true(is.list(df[[1]]$content))
+})
+
+# ---- get_klass_codes_single() ----
+
+test_that('get_klass_codes_single() fails correctly', {
+  expect_error(get_klass_codes_single(1789, 'municipality'))
+  expect_error(get_klass_codes_single('abc', 'municipality'))
 })
